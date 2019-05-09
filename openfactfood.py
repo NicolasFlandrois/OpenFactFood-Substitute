50e7f3d524a78c5fdd6598f8f7a70ffa790b5afb
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-09 11:13:34 +0200

    Previously raised issue fixed *args corrections

--- openfactfood.py
+++ openfactfood.py
@@ -6,7 +6,7 @@ class Question:
 	"""This class defines the menu and questions 
 	to navigate through the script."""
 	
-	def __init__(self, question, choices**):#Why the "choices**"
+	def __init__(self, question, *choices):
 		self.question = ""
 		self.choices =  []
 
@@ -37,15 +37,21 @@ class Question:
 q1 = "veuillez choisir une catégorie: "
 c1 = ['pâte à tartiner', 'confiture', 'coulis']
 #xyz2's questions & choices
-q2 = ""
-c2 = []
+q2 = "Question 2 : "
+c2 = ["A", "B", "C"]
 #xyz3's questions & choices
-q3 = ""
-c3 = []
+q3 = "Question 3"
+c3 = ["K", "L", "M"]
 #xyz4's questions & choices
-q4 = ""
-c4 = []
+q4 = "Question 4"
+c4 = ["X", "Y", "Z"]
 
 #TESTS LINES
-quest = Question()
-quest.menu(q1, c1)
+quest1 = Question(q1, c1)
+quest1.menu(q1, c1)
+quest2 = Question(q2, c2)
+quest2.menu(q2, c2)
+quest3 = Question(q3, c3)
+quest3.menu(q3, c3)
+quest4 = Question(q4, c4)
+quest4.menu(q4, c4)
