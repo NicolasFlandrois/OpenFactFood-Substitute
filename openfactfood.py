c12933e79ad56a8557f573ea1f7690d078420769
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-09 12:26:49 +0200

    Worked on cycles of questions
    What menu should appear depending on previous choice.

--- openfactfood.py
+++ openfactfood.py
@@ -9,6 +9,7 @@ class Question:
 	def __init__(self, question, *choices):
 		self.question = ""
 		self.choices =  []
+		self.c = None
 
 
 	def menu(self, question, choices):
@@ -31,27 +32,39 @@ class Question:
 					"Veuillez entrer un nombre entre 1 et " 
 					+ str(len(choices)) + "."
 					)
-		return choice
+		choicetxt = choices[choice-1]
+		print("Vous avez choisit: " + choicetxt)
+		self.c = str(choice)
 
 #Category menu's questions & choices
-q1 = "veuillez choisir une catégorie: "
-c1 = ['pâte à tartiner', 'confiture', 'coulis']
-#xyz2's questions & choices
-q2 = "Question 2 : "
-c2 = ["A", "B", "C"]
-#xyz3's questions & choices
-q3 = "Question 3"
-c3 = ["K", "L", "M"]
-#xyz4's questions & choices
-q4 = "Question 4"
-c4 = ["X", "Y", "Z"]
+c = None
+q0 = "Veuillez choisir une catégorie: "
+c0 = ['pâte à tartiner', 'confiture', 'coulis']
+#Products's questions & choices
+q1 = "veuillez choisir un produit : "
+c1 = []
+#Sustitution's questions & choices
+q2 = "Voulez vous substituer ce produit? "
+c2 = ["Oui", "Non"]
+#Substitutes Historic's questions & choices
+q3 = "Historique de substitution? "
+c3 = ["Oui", "Non"]
 
 #TESTS LINES
+quest0 = Question(q0, c0)
+quest0.menu(q0, c0)
+
+if quest0.c == "1":
+	c1 = ["Noisette Cacao", "Speculos", "Beurre de Cacaouette"]
+elif quest0.c == "2":
+	c1 = ["Confiture de Fraise", "Confiture d'Abricot", "Confiture d'Orange"]
+else:
+	c1 = ["Coulis de Fraise", "Miel", "Sirop d'érable"]
 quest1 = Question(q1, c1)
 quest1.menu(q1, c1)
+
 quest2 = Question(q2, c2)
 quest2.menu(q2, c2)
+
 quest3 = Question(q3, c3)
 quest3.menu(q3, c3)
-quest4 = Question(q4, c4)
-quest4.menu(q4, c4)
