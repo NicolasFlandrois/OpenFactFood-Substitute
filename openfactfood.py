6e89c2b8ecbc4728a15a55afa6e2d974e399c1aa
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-09 10:06:43 +0200

    Worked on classes
    defined the menu() function inside the class, and make it work, laying out questions sets in order to loop classes through them.

--- openfactfood.py
+++ openfactfood.py
@@ -6,32 +6,46 @@ class Question:
 	"""This class defines the menu and questions 
 	to navigate through the script."""
 	
-	def __init__(self, question, choices**):
-		#Stocker selfquestion & self choices
-		self.question = "veuillez choisir une catégorie: "
-		self.choices = ['pâte à tartiner', 'confiture', 'salé']
+	def __init__(self, question, choices**):#Why the "choices**"
+		self.question = ""#"veuillez choisir une catégorie: "
+		self.choices =  []#['pâte à tartiner', 'confiture', 'coulis']
 
 
-def menu(choices, question):
-	choice = None
+	def menu(self, question, choices):
+		choice = None
 
-	print(question)
+		print(question)
 
-	for num, choice in enumerate(choices):
-		print(str(num+1) + " : "+ choice)
+		for num, choice in enumerate(choices):
+			print(str(num+1) + " : "+ choice)
 
-	while True:
-		try:
-			choice = int(input())
-			if choice in range(1, len(choices)+1):
-				break
-			else:
-				raise
-		except:
-			print(
-				"Veuillez entrer un nombre entre 1 et " 
-				+ str(len(choices)) + "."
-				)
-	return choice
+		while True:
+			try:
+				choice = int(input())
+				if choice in range(1, len(choices)+1):
+					break
+				else:
+					raise
+			except:
+				print(
+					"Veuillez entrer un nombre entre 1 et " 
+					+ str(len(choices)) + "."
+					)
+		return choice
 
-menu(question.choices, question.question) #To optimise, & Make it work
+#Category menu's questions & choices
+q1 = "veuillez choisir une catégorie: "
+c1 = ['pâte à tartiner', 'confiture', 'coulis']
+#xyz2's questions & choices
+q2 = ""
+c2 = []
+#xyz3's questions & choices
+q3 = ""
+c3 = []
+#xyz4's questions & choices
+q4 = ""
+c4 = []
+
+#TESTS LINES
+quest = Question(q, c)
+quest.menu(q, c)
