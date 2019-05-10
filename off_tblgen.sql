6ff6220daf0332172f1dcba93da20e339e954ee5
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-10 17:12:29 +0200

    Script to fill DB from CSV
    SQL scripts to fill tables into DB, from data in CSV

--- off_tblgen.sql
+++ off_tblgen.sql
@@ -1,8 +1,10 @@
 -- Date: Fri 10 May 2019 16:43:14 CEST 
 -- Author: Nicolas Flandrois
 -- This script will create the initial database for OpenFoodFacts Porject#5
+
 CREATE DATABASE openfoodfacts CHARACTER SET 'utf8';
 
+--Table 1
 CREATE TABLE Categories ( 
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     category VARCHAR(40) NOT NULL,
@@ -10,17 +12,31 @@ CREATE TABLE Categories (
     origin_ean SMALLINT NOT NULL,
     substitute_ean SMALLINT NOT NULL,
     PRIMARY KEY (id)
-) --Table 1
+) 
 ENGINE=INNODB;
 
+LOAD DATA LOCAL INFILE 'tbl1_cat.csv'
+    INTO TABLE Categories
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    (category, subcategory, origin_ean, substitute_ean);
+
+--Table 2
 CREATE TABLE Products ( 
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     ean SMALLINT NOT NULL,
     product_name VARCHAR(40),
     PRIMARY KEY (id)
-) --Table 2
+) 
 ENGINE=INNODB;
 
+LOAD DATA LOCAL INFILE 'tbl2_prod.csv'
+    INTO TABLE Products
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    (ean, product_name);
+
+--Table 3
 CREATE TABLE History ( 
     id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     origin_ean SMALLINT NOT NULL,
@@ -29,5 +45,11 @@ CREATE TABLE History (
     date_change DATETIME NOT NULL,
     comments TEXT,
     PRIMARY KEY (id)
-) --Table 3
+) 
 ENGINE=INNODB;
+
+LOAD DATA LOCAL INFILE 'tbl3_historic.csv'
+    INTO TABLE History
+    FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    (origin_ean, substitute_ean, status, date_change, comments);
