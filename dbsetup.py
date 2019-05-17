8e6af9887566827f500a9857e921860b080cf4d8
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-17 12:40:19 +0200

    Test select from DB in order to append some usable lists in python
    multiple testings

--- dbsetup.py
+++ dbsetup.py
@@ -8,28 +8,19 @@ from auth import Auth
 auth = Auth()
  
 engine = al.create_engine('mysql+pymysql://' + auth.user + ':' + auth.password + '@' + auth.host + '/off1?host=localhost?port=3306', echo=True, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
-#need to select the database within mysql/mariadb and if not exist, 
-#create it during setup. 
+
 connection = engine.connect()
-connection.execute("SELECT host FROM INFORMATION_SCHEMA.PROCESSLIST WHERE ID = CONNECTION_ID()").fetchall()
 
-# base = sqlalchemy.ext.declarative.declarative_base()
-# metadata = base.metadata.create_all(engine)
-# print(metadata)
+#test
+# base = connection.execute("SHOW Tables").fetchall()
+# print(base)
+#Attempt to read a specific table, then enumerate and append a list/tuple in python
+# select = al.select('category').select_from('Categories')
+# results = connection.execute(select).fetchall()
 
-# def get_session(debug=False):
-#     engine = create_engine('mysql://root:pw@IP/DB', echo=debug, encoding='utf8', pool_recycle=300, pool_pre_ping=True)
-#     Base.metadata.create_all(engine)
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     try:
-#         yield session
-#         session.commit()
-#     except:
-#         session.rollback()
-#         raise
-#     finally:
-#         session.close()
+for row in connection.execute(al.select([Categories, products])):
+	print(row)
+
 
 # class Product(Base):
 #  	"""docstring for Product"""
