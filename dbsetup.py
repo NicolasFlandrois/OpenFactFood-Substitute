023003036604b8f4af7762b32f61e98f392e5a63
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-17 11:20:20 +0200

    MysqlAlchemy working connexion with the database
    Test on extracting info from the database and tables.
    Now, how to use it and extract what I need to use in python, then append the database?

--- dbsetup.py
+++ dbsetup.py
@@ -6,12 +6,16 @@
 import sqlalchemy as al
 from auth import Auth
 auth = Auth()
-
-engine = al.create_engine('mysql://'+auth.user+'@'+auth.host+'/off1:'+auth.port,
-echo=True) 
+ 
+engine = al.create_engine('mysql+pymysql://' + auth.user + ':' + auth.password + '@' + auth.host + '/off1?host=localhost?port=3306', echo=True, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
 #need to select the database within mysql/mariadb and if not exist, 
 #create it during setup. 
 connection = engine.connect()
+connection.execute("SELECT host FROM INFORMATION_SCHEMA.PROCESSLIST WHERE ID = CONNECTION_ID()").fetchall()
+
+# base = sqlalchemy.ext.declarative.declarative_base()
+# metadata = base.metadata.create_all(engine)
+# print(metadata)
 
 # def get_session(debug=False):
 #     engine = create_engine('mysql://root:pw@IP/DB', echo=debug, encoding='utf8', pool_recycle=300, pool_pre_ping=True)
@@ -27,12 +31,12 @@ connection = engine.connect()
 #     finally:
 #         session.close()
 
-class Product(Base):
- 	"""docstring for Product"""
- 	__tablename__ = "products"
- 	id = Column(Integer, primary_key=True)
- 	category = Column(Varchar)
- 	def __init__(self, arg):
- 		super(Product, self).__init__()
- 		self.arg = arg
- 		 Models(self):
+# class Product(Base):
+#  	"""docstring for Product"""
+#  	__tablename__ = "products"
+#  	id = Column(Integer, primary_key=True)
+#  	category = Column(Varchar)
+#  	def __init__(self, arg):
+#  		super(Product, self).__init__()
+#  		self.arg = arg
+#  		 Models(self):
