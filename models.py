adaa86ee925f76c0d616a25541b43a5feea48989
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-20 17:25:46 +0200

    Merge's clean up

--- models.py
+++ models.py
@@ -1,20 +1,18 @@
-
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base  
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-<<<<<<< HEAD
 
 engine = create_engine(
 	'mysql+pymysql://odin:lincoln@localhost/off1?host=localhost?port=3306', 
 	echo=True, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
-=======
+
 from auth import Auth
 auth = Auth()
 
-engine = create_engine('mysql+pymysql://' + auth.user + ':' + auth.password + '@' + auth.host + '/off1?host=localhost?port=3306', echo=True, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
->>>>>>> master
+engine = create_engine('mysql+pymysql://odin:lincoln@localhost/off1?host=localhost?port=3306', echo=True, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
+
 Session = sessionmaker(bind=engine)
 session = Session()
 
