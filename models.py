3f87a85ecfa32fc0c43d7403f42b0bec10a9ed98
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-11 22:48:35 +0200

    First PEP8 cleaning @ models.py

--- models.py
+++ models.py
@@ -1,18 +1,18 @@
 #!usr/bin/python3.5
 # UTF8
-# Date: Thu 09 May 2019 14:40:35 CEST 
+# Date: Thu 09 May 2019 14:40:35 CEST
 # Author: Nicolas Flandrois
 
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy import create_engine, MetaData
-from sqlalchemy.ext.declarative import declarative_base  
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 engine = create_engine(
-	'mysql+pymysql://odin:lincoln@localhost/off1?host=localhost?port=3306', 
-	echo=False, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
+    'mysql+pymysql://odin:lincoln@localhost/off1?host=localhost?port=3306',
+    echo=False, encoding='utf8', pool_recycle=60000, pool_pre_ping=True)
 
 
 Session = sessionmaker(bind=engine)
@@ -20,28 +20,23 @@ session = Session()
 
 metadata = MetaData()
 
-class Product(Base):
-	"""docstring for Products"""
-	__tablename__ = "product"
-	id = Column(Integer, primary_key=True)
-	ean = Column(String(13), nullable=False)
-	name = Column(String(50))
-	category = Column(Integer, ForeignKey('category.id'))
-	substitute = Column(Integer, ForeignKey('product.id'))
-	substituted = Column(Boolean)
 
-	# def __repr__(self):
-	# 	return str((self.ean))
-	# 	return str((self.product_name))
-	# 	return str((self.category))
-	# 	return str((self.substitute))
-	# 	return str((self.substituted))
-	
+class Product(Base):
+    """docstring for Products"""
+    __tablename__ = "product"
+    id = Column(Integer, primary_key=True)
+    ean = Column(String(13), nullable=False)
+    name = Column(String(50))
+    category = Column(Integer, ForeignKey('category.id'))
+    substitute = Column(Integer, ForeignKey('product.id'))
+    substituted = Column(Boolean)
+
+
 class Category(Base):
-	"""docstring for Categories"""
-	__tablename__ = "category"
-	id = Column(Integer, primary_key=True)
-	label = Column(String(50))
-	
-	def __repr__(self):
-		return str((self.label))
+    """docstring for Categories"""
+    __tablename__ = "category"
+    id = Column(Integer, primary_key=True)
+    label = Column(String(50))
+
+    def __repr__(self):
+        return str((self.label))
