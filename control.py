74b74a7146680f218f35790e0c6d367160f4c6bd
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-11 23:18:13 +0200

    PEP8 Control.py

--- control.py
+++ control.py
@@ -1,18 +1,19 @@
 #!usr/bin/python3.5
 # UTF8
-# Date: Thu 09 May 2019 14:40:35 CEST 
+# Date: Thu 09 May 2019 14:40:35 CEST
 # Author: Nicolas Flandrois
 
 from models import Product, Category
 from view import View
 
+
 def main():
-	"""docstring ici"""
-	category_id = View.categories_list()
-	prod_id = View.products_list(category_id)
-	View.product_sheet(prod_id)
-	View.prod_sub(prod_id)
+    """docstring ici"""
+    category_id = View.categories_list()
+    prod_id = View.products_list(category_id)
+    View.product_sheet(prod_id)
+    View.prod_sub(prod_id)
 
 
 if __name__ == '__main__':
-	main()
+    main()
