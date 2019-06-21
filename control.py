7820d4c49e25c729cb0454ac7405c0855974c385
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-21 13:32:30 +0200

    Creating a sequence of events that will be the final prog - Issue Raised
    I first created a function to manage Quit and Return, but as it's def outside of while loop, error raised:
      File "control.py", line 27
        continue
        ^
    SyntaxError: 'continue' not properly in loop

--- control.py
+++ control.py
@@ -5,28 +5,62 @@
 
 import os 
 import platform
+from sys import exit
 from models import Product, Category
 from view import View
 
 def clean():
     """This function will clear the terminal's screen. The command is 
     automaticaly detected according to the system OS you run it."""
-    print("(Appuyez sur Entrer pour continuer)")
-    input()
     if platform.system() == "Windows":
         os.system("cls")
     else:
         os.system("clear") #This command will work on Linux and OSx systems.
 
+def return_or_quit(responce):
+    """This function will either Quit the programm, or Return to main menu."""
+    if responce == 'r':
+        pass
+    elif responce == 'q':
+        exit()
+    else:
+        continue
+
 def main():
     """Main running function."""
-    category_id = View.categories_list()
-    prod_id = View.products_list(category_id)
-    View.product_sheet(prod_id)
-    clean()
-    View.prod_sub(prod_id)
-    clean()
-    View.sub_tbl()
+    print("Bienvenu dans ce programme.")
+    while True:
+        responce = View.main_menu()
+        clean()
+        if responce in ["r", "q"]:
+            return_or_quit(responce)
+        elif responce == 1:
+            category_id = View.categories_list()
+            return_or_quit(category_id)
+            prod_id = View.products_list(category_id)
+            return_or_quit(prod_id)
+            clean()
+            
+            View.product_sheet(prod_id)
+            nextmove = input("(Appuyez sur Entrer pour continuer)")
+            return_or_quit(nextmove)
+            clean()
+            
+            choice = View.sub_menu()
+            return_or_quit(choice)
+            clean()
+            
+            if choice == 1:
+                View.prod_sub(prod_id)
+                # No return_or_quit The choice Not to substitute 
+                # needs to be deliberate
+                clean()
+            else:
+                pass
+        else:
+            View.sub_tbl()
+            print("\n\n Retour au menu principale.")
+            clean()
 
 
 if __name__ == '__main__':
