73de30e6521be8e78c00781a49eba90f238c9fa7
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-06-28 11:52:08 +0200

    Solved all issues in view.py refactoring == OK
    Beta test line by line = Working condition == OK

--- view.py
+++ view.py
@@ -54,7 +54,7 @@ R pour RETOUR au menu principal.)\n')
         return choice
     
     @staticmethod
-    def get_off_json(ean):
+    def get_off_json(ean:str):
         """Get info from Open Fact Food (off) URL's Json API. Returns a tuple."""
         with urlopen(f"https://world.openfoodfacts.org/api/v0/product/{ean}.json") as response:
             source = response.read()
@@ -73,27 +73,28 @@ R pour RETOUR au menu principal.)\n')
             return quantity, ingredients # returns a tuple
 
     @staticmethod
-    def status(substituted):
+    def status(substituted:bool):
         """Translate the Substituted status True/False into a Yes/No string."""
         return "Oui" if substituted is True  else "Non"
 
     @staticmethod
-    def sheet_structure(name, ean, quantity, ingredients, subname, substatus):
+    def sheet_view(prod_id:int):
         """Returns the display for the product's sheet."""
+        prod_details = View.prod_details(prod_id)
         print(
-            f"Le produit selectionné est:       {name}. \n\
-EAN-13:                           {ean}. \n\
-Poids:                            {quantity}. \n\
+            f"Le produit selectionné est:       {prod_details['name']}. \n\
+EAN-13:                           {prod_details['ean']}. \n\
+Poids:                            {prod_details['quantity']}. \n\
 \n\
 Liste d'ingrédients:\n\
-                    {ingredients}. \n\
+                    {prod_details['ingredients']}. \n\
 \n\
-Son substitue est:                {subname}. \n\
-Ce produit est-il déjà substitué? {substatus}. \n"
+Son substitue est:                {prod_details['subname']}. \n\
+Ce produit est-il déjà substitué? {prod_details['substatus']}. \n"
             )
 
     @staticmethod
-    def substitution(product_id):
+    def substitution(product_id:bool):
         """Action to apply Substitution"""
         get_prod(product_id).substituted = True
         get_prod(product_id).substitute.substituted = False
@@ -101,7 +102,7 @@ Ce produit est-il déjà substitué? {substatus}. \n"
         return "La substitution a bien été enregistrer."
 
     @staticmethod
-    def sub_tbl_structure(prod_name, sub_name):
+    def sub_tbl_structure(prod_name:str, sub_name:str):
         """This function will display the list of all substituted products in
         the database, along with its matching substitute.
         Data a from real time database."""
@@ -134,23 +135,22 @@ Ce produit est-il déjà substitué? {substatus}. \n"
     )
 
     # Product's Sheet
-    product_call = lambda prod_id: session.query(Product).filter(Product.category == prod_id)
+    product_call = lambda prod_id: session.query(Product).filter(Product.id == prod_id)
     
-    def product2sheet(prod_id):
-        prod_details = []
-        for label in View.product_call(prod_id):
-            prod_details.append(label)
-        return prod_details
+    def prod_details(prod_id:int):
+        """Get all needed prod details for the product's sheet."""
+        prod_detail = {}
+        prod_detail['name'] = "".join([prod.name for prod in View.product_call(prod_id)])
+        prod_detail['ean'] = "".join([prod.ean for prod in View.product_call(prod_id)])
+        json_detail = View.get_off_json(prod_detail['ean'])
+        prod_detail['quantity'] = json_detail[0]
+        prod_detail['ingredients'] = json_detail[1]
+        prod_detail['subid'] = int(''.join(map(str,[prod.substitute for prod in View.product_call(prod_id)]))) 
+        prod_detail['subname'] = "".join([prod.name for prod in View.product_call(prod_detail['subid'])])
+        prod_detail['substatus'] = View.status([prod.substituted for prod in View.product_call(prod_id)])
+        return prod_detail
 
-    prod_view = lambda prod_id: View.sheet_structure(
-                    View.product2sheet(prod_id)[2],
-                    View.product2sheet(prod_id)[1],
-                    (View.get_off_json(View.product2sheet(prod_id)[1])[0]),
-                    (View.get_off_json(View.product2sheet(prod_id)[1])[1]),
-                    View.product_call(View.product2sheet(prod_id)[4]),
-                    (View.status(View.product2sheet(prod_id)[5]))
-                )
-
+    # Submenu View To choice either we shall substitute or not this product
     submenu_view = lambda: View.menu(
                                     "Voulez-vous :", [
                                         "Substituer ce produit ?",
@@ -158,31 +158,29 @@ Ce produit est-il déjà substitué? {substatus}. \n"
                                        ]
                                     )
 
-    # Preparing lists for substitution table
-    sub_prodnames = [prod.name for prod in session.query(Product).filter(Product.substituted == True)]
-
-    sub_subid = [prod.substitute for prod in session.query(Product).filter(Product.substituted == True)]
-
-    sub_subnames = [[prod.name for prod in session.query(Product).filter(Product.id == subid)] 
-                            for subid in sub_subid]
-
-    sub_prodnames_subnames = list(zip(sub_prodnames, sub_subnames))
-
+    # View of substitution table
     @staticmethod
-    def sub_tbl (prod_sub_names:list):
+    def sub_tbl ():
+        """View of substitution table"""
+        sub_prodnames = [prod.name for prod in session.query(Product).filter(Product.substituted == True)]
+
+        sub_subid = [prod.substitute for prod in session.query(Product).filter(Product.substituted == True)]
+
+        sub_subnames = ["".join([prod.name for prod in session.query(Product).filter(Product.id == subid)]) 
+                                for subid in sub_subid]
+
+        sub_prodnames_subnames = list(zip(sub_prodnames, sub_subnames))
+
         print("Liste des produits substitués : ")
-        for item in prod_sub_names:
+        
+        for item in sub_prodnames_subnames:
             print(View.sub_tbl_structure(item[0], item[1]))
 
 # TEST
 # View.main_view() # ok
 # View.submenu_view() # ok
-# print(View.sub_prodnames_subnames) # OK
-# View.sub_tbl(View.sub_prodnames_subnames) # OK
+# View.sub_tbl() # OK
 # View.cats_view() # OK
 # View.prods_view(2) # OK
 
-# print(View.product2sheet(2))
-
-View.prod_view(2)
-# Issues
+View.sheet_view(2)
